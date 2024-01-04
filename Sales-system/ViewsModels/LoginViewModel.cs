6c1a30a6449c7f1c3e6ce36292685f82752287db
Gabriel Vieira <gv.santos@sidi.org.br>
2024-01-04 17:24:27 -0300

    Obtendo os dados inseridos na interface de login

--- Sales-system/ViewsModels/LoginViewModel.cs
+++ Sales-system/ViewsModels/LoginViewModel.cs
@@ -1,17 +1,41 @@
-﻿using Sales_system.Models;
+﻿using Sales_system.Library;
+using Sales_system.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 namespace Sales_system.ViewModels
 {
   public class LoginViewModel : UserModel
   {
+    private ICommand _command;
+    private TextBox _textBoxEmail;
+    private PasswordBox _textBoxPass;
+    private String date = DateTime.Now.ToString("dd/MMM/yyy");
+    private Frame rootFrame = Window.Current.Content as Frame;
     public LoginViewModel(object[] campos)
     {
-
+      _textBoxEmail = (TextBox)campos[0];
+      _textBoxPass = (PasswordBox)campos[1];
+    }
+    public ICommand IniciarCommand
+    {
+      get
+      {
+        return _command ?? (_command = new CommandHandler(async () =>
+        {
+          await iniciarAsync();
+        }));
+      }
+    }
+    private async Task iniciarAsync()
+    {
+      var data = Email;
     }
   }
 }
